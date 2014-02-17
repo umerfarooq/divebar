dbbb1e12c3ac0024a6d46c8ee50a8732c9a439e3
Umer Farooq <mail.to.umerfarooq@gmail.com>
2014-02-17 19:44:34 +0500

    Added Google Maps on Locations Pages

--- wp-content/themes/divebar/category.php
+++ wp-content/themes/divebar/category.php
@@ -118,7 +118,14 @@
                             <p>
                                 <?php the_field('directions') ?>
                             </p>
-                            <div class="map"></div>
+                            <?php
+                            $location = get_field('map_location');
+                            if (!empty($location)):
+                                ?>
+                                <div class="acf-map">
+                                    <div class="marker" data-lat="<?php echo $location['lat']; ?>" data-lng="<?php echo $location['lng']; ?>"></div>
+                                </div>
+                            <?php endif; ?>
                         </li>
                         <li class="get-touch">
                             <h1>Get in Touch</h1>
